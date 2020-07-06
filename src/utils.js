da357596c3fd1fe11fad40434d016280d69a9e85
Usman Tahir <usmanporsnal786@gmail.com>
2020-07-06 22:15:47 +0500

    Added a Navebar and news feed

--- src/utils.js
+++ src/utils.js
@@ -18,9 +18,9 @@ export async function initContract() {
   // Initializing our contract APIs by contract name and configuration
   window.contract = await near.loadContract(nearConfig.contractName, { // eslint-disable-line require-atomic-updates
     // View methods are read only. They don't modify the state, but usually return some value.
-    viewMethods: ['getGreeting'],
+    viewMethods: ['getAllPosts','getPostById'],
     // Change methods can modify the state. But you don't receive the returned value when called.
-    changeMethods: ['setGreeting'],
+    changeMethods: ['createPost'],
     // Sender is the account ID to initialize transactions.
     sender: window.accountId,
   })
@@ -32,22 +32,17 @@ export async function onSubmit(event) {
   event.preventDefault()
 
   // get elements from the form using their id attribute
-  const { fieldset, greeting } = event.target.elements
+  const { postTitle, postContent } = event.target.elements
 
-  // disable the form while the value gets updated on-chain
-  fieldset.disabled = true
-
+  
   // make an update call to the smart contract
-  await contract.setGreeting({
+  await window.contract.createPost({
     // pass the value that the user entered in the greeting field
-    message: greeting.value
+    title: postTitle.value,
+    body: postContent.value
   })
 
-  // re-enable the form
-  fieldset.disabled = false
-
-  // disable the save button, since it now matches the persisted value
-  document.querySelector('form button').disabled = true
+  
 }
 
 export function logout() {
@@ -61,6 +56,6 @@ export function login() {
     // The contract name that would be authorized to be called by the user's account.
     nearConfig.contractName,
     // This is the app name. It can be anything.
-    'Welcome to NEAR'
+    'NEAR-ONES'
   )
 }
